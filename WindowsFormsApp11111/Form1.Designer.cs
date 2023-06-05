98949f880ff4abdbc5091e8d8722254869ebae3f
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-06-06 04:34:33 +0700

    Add files via upload

---
+++ WindowsFormsApp11111/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApp11111
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 69);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(236, 129);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Location = new System.Drawing.Point(236, 101);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 129);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(131, 18);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Число механиков";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 450;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(362, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(537, 353);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Кто";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Что делает";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(440, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 25);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Машин в очереди на починку:  0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 18);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Интенсивность машин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 18);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Интенсивность механиков";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Location = new System.Drawing.Point(236, 73);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown3.TabIndex = 19;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 428);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ WindowsFormsApp11111/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11111
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Mechanic
+        {
+            public int car = 0;
+            public int n;
+            double mu = 1.5;
+
+            public Mechanic(int i)
+            {
+                n = i;
+            }
+
+            public double work_time(Random random)
+            {
+                double a = random.NextDouble();
+
+                return -Math.Log(a) / mu;
+            }
+        }
+
+        public class Autoservise
+        {
+            public List<Mechanic> mechanics = new List<Mechanic>();
+            Random random = new Random();
+            int N, car_number;
+            public int queue;
+            double work_intensity;
+            double car_intensity;
+
+            public Autoservise(int n, double intensity2, double intensity1)
+            {
+                N = n;
+                work_intensity = intensity2;
+                car_intensity = intensity1;
+
+                for (int i = 0; i < n; i++)
+                {
+                    mechanics.Add(new Mechanic(i));
+                }
+            }
+
+            public double Time(double b = -1)
+            {
+                if (b == -1)
+                {
+                    b = car_intensity;
+                }
+
+                double a = random.NextDouble();
+
+                return -Math.Log(a) / b;
+            }
+
+            public void mTime(double t1)
+            {
+                double work_time;
+                int working = 0;
+
+                for (int i = 0; i < N; i++)
+                {
+                    if (mechanics[i].car != 0)
+                    {
+                        working++;
+                    }
+                }
+
+                if (working > 0)
+                {
+                    work_time = Time(work_intensity * working);
+                }
+                else
+                {
+                    work_time = Int64.MaxValue;
+                }
+
+                if (t1 < work_time)
+                {
+                    if (working < N)
+                    {
+                        mechanics[Vacant()].car = car_number;
+                        car_number++;
+                    }
+                    else
+                    {
+                        queue++;
+                    }
+                }
+                else
+                {
+                    int i = 0;
+                    double t = Int64.MaxValue / 2;
+
+                    foreach (Mechanic vasya in mechanics)
+                    {
+                        if (vasya.car != 0)
+                        {
+                            double new_t = vasya.work_time(random);
+
+                            if (new_t < t)
+                            {
+                                i = vasya.n; 
+                                t = new_t;
+                            }
+                        }
+                    }
+
+                    int car = 0;
+
+                    if (queue > 0)
+                    {
+                        car = car_number;
+                        car_number++;
+                        queue--;
+                    }
+
+                    mechanics[i].car = car;
+                }
+            }
+
+            public int Vacant()
+            {
+                int i = 0;
+
+                for (i = 0; i < N; i++)
+                {
+                    if (mechanics[i].car == 0)
+                    {
+                        break;
+                    }
+                }
+
+                return i;
+            }
+        }
+
+        Autoservise autoservise;
+        double intensity1, intensity2;
+        int n;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            n = (int)numericUpDown1.Value;
+            intensity1 = (double)numericUpDown3.Value;
+            intensity2 = (double)numericUpDown2.Value;
+
+            autoservise = new Autoservise(n, intensity2, intensity1);
+
+            button1.Text = "Обновить входные данные";
+
+            timer1.Start();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+
+            autoservise.mTime(autoservise.Time());
+
+            label1.Text = "Машин в очереди на починку: " + autoservise.queue.ToString();
+
+            foreach (Mechanic mechanic in autoservise.mechanics)
+            {
+                if (mechanic.car != 0)
+                {
+                    dataGridView1.Rows.Add("Механик №" + (mechanic.n + 1), "Обслуживает машину № " + mechanic.car.ToString());
+                }
+                else
+                {
+                    dataGridView1.Rows.Add("Механик №" + (mechanic.n + 1), "Ничего не делает");
+                }
+            }
+        }
+    }
+}
